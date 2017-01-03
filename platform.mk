807da29c55c9fa2d8f9dd6bc564e21c43c85b20b
David <Davidteri91@gmail.com>
2017-01-03 12:08:09 +0100

    shinano: common --> common-aosp
    needed for LOS
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- platform.mk
+++ platform.mk
@@ -16,7 +16,7 @@
 PLATFORM_COMMON_PATH := device/sony/shinano-common
 
 $(call inherit-product, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
-$(call inherit-product, device/sony/common/common.mk)
+$(call inherit-product, device/sony/common-aosp/common.mk)
 
 SOMC_PLATFORM := shinano
 
